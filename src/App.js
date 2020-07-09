d1729318a9e0c25929d1fe8a4912e303d5a37a31
Naman Jindal <namanjindal113@gmail.com>
2020-07-09 16:54:50 +0530

    added update, delete and some queries to firebase

--- src/App.js
+++ src/App.js
@@ -2,32 +2,43 @@ import React from 'react';
 import Cart from './Cart';
 import Navbar from './Navbar';
 import * as firebase from 'firebase';
+
 class App extends React.Component {
 constructor(){
 	super();
 	this.state = {
 		products:[],
-		loader: true
+		loader: true,
+
 	}
+	this.db = firebase.firestore();
 }
 componentDidMount(){
 	console.log("in component did mount");
-	firebase
-		.firestore()
+	this.db
 		.collection('products')
+		//we can query data in many ways -  sorting(), get specific data
+		//.where('price', "==", 999)
+		//we can also nest queries - applying more than 1 condition
+		//.where('title', '==', 'Mug')
+
+		//we can also sort by using
+		//asc or desc - asc by default
+		//.orderBy('price', 'asc')
+
 		//.onsnapshot is a method given by firebase, it is basically a listener on the database, whenever database updates this
 		//onsnapshot is fired again, and hence our state is set with the new updated producgt
 		.onSnapshot((snapshot) => {
-			console.log(snapshot);
+			//console.log(snapshot);
 			snapshot.docs.map((doc)=>{
-				console.log("messgae doc", doc.data());
+				//console.log("messgae doc", doc.data());
 			})
 			const products = snapshot.docs.map((doc) => {
 				const data = doc.data();
 				data['id']= doc.id
 				return data;
 			})
-			console.log(products);
+			//.log(products);
 
 			this.setState({
 				products,
@@ -59,29 +70,65 @@ handleIncreaseQuantity = (product) =>{
 	const {products} = this.state;
 	//now we have to make changes in particular product, so we need to find that in all my proudcts
 	const index = products.indexOf(product);
-	products[index].qty += 1;
-	this.setState({
-		products
+	/////this was used to update in non persistent data
+	// products[index].qty += 1;
+	// this.setState({
+	// 	products
+	// })
+
+	//updating through firebase
+	const docRef = this.db.collection('products').doc(products[index].id);
+	console.log(products[index].id);
+	docRef.update({
+		qty: products[index].qty +1
+	})
+	.then(()=>{
+		console.log("product updated succesfully");
+	})
+	.catch((err)=>{
+		console.log("error updating the product", err);
 	})
 }
 handleDecreaseQuantity = (product) =>{
-	console.log("called");
+	//console.log("called");
 	const {products} = this.state;
 	if(product.qty>0){
 		const index = products.indexOf(product);
-		products[index].qty -= 1;
-		this.setState({
-			products
+		// products[index].qty -= 1;
+		// this.setState({
+		// 	products
+		// })
+		//updating through firebase
+		const docRef = this.db.collection('products').doc(products[index].id);
+		console.log(products[index].id);
+		docRef.update({
+			qty: products[index].qty +1
+		})
+		.then(()=>{
+			console.log("product updated succesfully");
+		})
+		.catch((err)=>{
+			console.log("error updating the product", err);
 		})
 	}
 	
 }
 deleteProduct = (id) =>{
 	const {products} = this.state;
-	const items = products.filter((item)=> item.id !== id); // returns an array with  products except for the once which has the id that we passed
-	this.setState({
-		products: items
-	})
+	// const items = products.filter((item)=> item.id !== id); // returns an array with  products except for the once which has the id that we passed
+	// this.setState({
+	// 	products: items
+	// })
+	const docRef = this.db.collection('products').doc(id);
+		
+		docRef
+		.delete()
+		.then(()=>{
+			console.log("deleted successfully");
+		})
+		.catch((err)=>{
+			console.log("error deleting the product", err);
+		})
 }
 test = () =>{
 	console.log("naman");
@@ -105,6 +152,24 @@ getCartTotal = () => {
     })
 	return cartTotal;
   }
+
+addProduct = () => {
+	this.db
+		.collection('products')
+		.add({
+			img: '',
+			price: 12321,
+			qty: 2,
+			title: "Washing Machine"
+		})
+		.then((docRef) => {
+			console.log("product has been added",docRef);
+
+		})
+		.catch((error) => {
+			console.log(error);
+		})
+}
 render(){
 	const {products, loader} = this.state; 
 	return (
@@ -112,6 +177,7 @@ render(){
 		<Navbar 
 			count = {this.getCartCount()}
 		/>
+		<button onClick={this.addProduct} style={{padding: 20, fontSize: 20}}>Add a Product</button>
 		<Cart 
 			products = {products}
 			onIncreaseQuantity= {this.handleIncreaseQuantity}
